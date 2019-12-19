911a47c84005efcc39934704420fc47f6570e1b3
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-12-19 17:10:05 -0500

    Apply APE 17 :fire:
    Bye astropy-helpers

--- stsynphot/commissioning/__init__.py
+++ stsynphot/commissioning/__init__.py
@@ -9,32 +9,16 @@
 To run only the commissioning tests from source checkout::
 
     cd ../..
-    python setup.py test -P commissioning --remote-data --args="--slow"
+    pytest --pyargs stsynphot/commissioning --remote-data --slow
 
 Like above, but also use ``pytest-html`` plugin for a detail HTML report::
 
-    python setup.py test -P commissioning --remote-data \
-    --args="--slow --html=/full/path/report.html"
-
-To generate the HTML report using ``py.test`` directly::
-
-    py.test stsynphot/commissioning/tests/ --remote-data \
-    --slow --html=report.html
-
-To run only the commissioning tests from a Python session::
-
-    >>> import stsynphot
-    >>> stsynphot.test('commissioning', remote_data=True, args='--slow')
-
-Like above, but also use ``pytest-html`` plugin for a detail HTML report::
-
-    >>> stsynphot.test('commissioning', remote_data=True,
-    ...                args='--slow --html="report.html"')
+    pytest --pyargs stsynphot/commissioning --remote-data --slow --html=/full/path/report.html
 
 To rerun only failed tests from the last run, add ``--lf`` in the list of
 ``args`` above. This option requires ``pytest-cache`` (enabled by default)
 and is useful for debugging.
 
-"""
+"""  # noqa
 
 from . import utils  # noqa
