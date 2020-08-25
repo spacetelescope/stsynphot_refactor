53492df9324d38044bddc3a244ac9c6ed5f2b5ef
P. L. Lim <2090236+pllim@users.noreply.github.com>
2020-08-25 10:41:50 -0400

    TST: NASA blocking CI linkcheck

--- docs/conf.py
+++ docs/conf.py
@@ -134,6 +134,7 @@ linkcheck_ignore = ['https://hsthelp.stsci.edu',
                     'http://ssb.stsci.edu',
                     'http://www.stsci.edu',
                     'https://www.as.arizona.edu/observing',
-                    'https://phoenix.ens-lyon.fr/simulator/index.faces']
+                    'https://phoenix.ens-lyon.fr/simulator/index.faces',
+                    'https://wfirst.gsfc.nasa.gov/science/WFIRST_Reference_Information.html']
 linkcheck_timeout = 180
 linkcheck_anchors = False
